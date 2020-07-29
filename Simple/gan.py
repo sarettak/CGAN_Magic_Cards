740df7c32cc53f62f7c69d0ed4734d666b7a3572
Emilian Postolache <emilianpos@gmail.com>
2020-07-29 03:11:32 +0200

    added global_step in tensorboard

--- Simple/gan.py
+++ Simple/gan.py
@@ -192,7 +192,7 @@ for epoch in range(opt.n_epochs):
                 #         'height': 512
                 # },
                 #     win=1)
-                writer.add_images("samples_1", imgs_fake.data[:24])
+                writer.add_images("samples_1", imgs_fake.data[:24], global_step=batches_done)
               
 
                 # vis.images(
