5a0c0351e189f6e1f336905bc1cab1ca30a45bab
Emilian Postolache <emilianpos@gmail.com>
2020-07-30 04:00:50 +0200

    added multihinge script

--- BigGAN-PyTorch/scripts/final/launch_cifar_mhingegan.sh
+++ BigGAN-PyTorch/scripts/final/launch_cifar_mhingegan.sh
@@ -3,16 +3,16 @@ python train.py \
 --shuffle --batch_size 50 --parallel \
 --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 \
 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 \
---dataset C10 \
+--dataset Magic \
 --G_ortho 0.0 \
 --G_attn 0 --D_attn 0 \
 --G_init N02 --D_init N02 \
 --ema --use_ema --ema_start 1000 \
---save_every 500 --num_best_copies 5 --num_save_copies 2 --seed 0 \
---data_root /fs/vulcan-scratch/ilyak/locDoc/data/cifar10 \
---weights_root /fs/vulcan-scratch/ilyak/locDoc/experiments/biggancifar \
---logs_root /fs/vulcan-scratch/ilyak/locDoc/experiments/biggancifar \
---samples_root /fs/vulcan-scratch/ilyak/locDoc/experiments/biggancifar \
+--save_every 100 --num_best_copies 5 --num_save_copies 2 --seed 0 \
+--data_root data \
+--weights_root logs \
+--logs_root logs \
+--samples_root samples \
 --test_every -1 \
 --experiment_name mhs_p05 \
 --historical_save_every 1000 \
