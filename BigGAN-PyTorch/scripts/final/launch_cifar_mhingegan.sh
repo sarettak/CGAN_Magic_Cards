923c823596b41ae9aa45eafe6468b5bd763e5760
Emilian Postolache <emilianpos@gmail.com>
2020-07-30 04:59:31 +0200

    load_in_mem on mhingegan

--- BigGAN-PyTorch/scripts/final/launch_cifar_mhingegan.sh
+++ BigGAN-PyTorch/scripts/final/launch_cifar_mhingegan.sh
@@ -5,6 +5,7 @@ python train.py \
 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 \
 --dataset Magic \
 --G_ortho 0.0 \
+--load_in_mem \
 --G_attn 0 --D_attn 0 \
 --G_init N02 --D_init N02 \
 --ema --use_ema --ema_start 1000 \
