0a8ce8bbe82ddd7e54bfe9656e34079851a150a5
Emilian Postolache <emilianpos@gmail.com>
2020-07-31 04:07:55 +0200

    fixed 128 gifs and those with <= 1000

--- experiment/animate.py
+++ experiment/animate.py
@@ -9,8 +9,13 @@ for subdir, dirs, files in os.walk(root):
         for file_ in files:
             if file_.endswith(".jpg"):
                 image_path = os.path.join(subdir, file_)
-                images.append((image_path, imageio.imread(image_path)[:465, :, :]))
+                image = imageio.imread(image_path)
+                if "128" in subdir:
+                    image = image[:910, :, :] 
+                else:
+                    image = image[:465, :, :]
+                images.append((image_path, image))
         images.sort(key=lambda it: int(it[0].split("fixed_samples")[1].split(".jpg")[0]))
         images = [im for _, im in images]
-        imageio.mimsave(os.path.join(subdir, "anim.gif"), images, duration=0.4)
+        imageio.mimsave(os.path.join(subdir, "anim.gif"), images, duration=0.2)
         
